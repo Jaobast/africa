415fce08924c27b7c146b11f39e38667be6f2267
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-11-09 00:13:37 +0100

    init

---
+++ script.js
@@ -0,0 +1,48 @@
+const word = document.getElementById("word");
+const submit = document.getElementById("submit");
+const list = document.getElementById("list-words");
+const deleteAll = document.getElementById("delete-all");
+
+let wordList = [];
+let number = 0;
+
+// Função para adicionar o conteúdo da lista no DOM
+function addWordToList(word) {
+    const newWordToList = document.createElement("p");
+    newWordToList.innerHTML = word;
+    list.appendChild(newWordToList);
+}
+
+submit.addEventListener('click', (e) => {
+    wordList.push(word.value);
+    addWordToList(word.value);
+    
+    localStorage.setItem('list', JSON.stringify(wordList));
+    
+    word.value = "";
+    word.focus();
+    number++;
+});
+
+// Função para carregar a lista do localStorage e mostrar no DOM
+function localList() {
+    const itemLocalList = localStorage.getItem('list');
+    
+    if (itemLocalList) {
+        wordList = JSON.parse(itemLocalList);
+        
+        // Exibe cada item da lista no DOM
+        wordList.forEach(word => {
+            addWordToList(word);
+        });
+    }
+}
+
+deleteAll.addEventListener('click', (e) => {
+    localStorage.clear();
+    list.innerHTML = "";  // Também limpa a exibição atual na página
+    wordList = [];        // Limpa a lista em memória
+});
+
+// Carrega a lista assim que a página é carregada
+localList();
