7eed4e01983d399aad063602ae78f91593384757
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-11-26 12:16:47 +0100

    name-list init

--- script/new-person.js
+++ script/new-person.js
@@ -3,28 +3,22 @@ const group02 = document.getElementById('group2');
 
 const containerPerson = document.querySelector('.person-container');
 const containerGroup = document.querySelector('.group-container');
-const bttnPerson = document.getElementById('person-done');
+const bttnPersonDone = document.getElementById('person-done');
 
 const inputName = document.getElementById('name');
 const bttnName = document.getElementById('send-name');
 const namePlaceholder = document.querySelector('.name-placeholder');
 const newWordList = document.querySelector('.new-word-list');
 
+const nameList = document.querySelector('.name-list');
+
 let phraseArray = [];
 let currentPerson = null;
 
+let peopleArray = [];
+
 group01.addEventListener('click', () => startNewPerson());
 group02.addEventListener('click', () => startNewPerson());
-
-bttnPerson.addEventListener('click', () => {
-    if (currentPerson) {
-        console.log(`Objeto fechado para ${currentPerson}`);
-    }
-    currentPerson = null; // "Fecha" o objeto atual
-    hiddenDiv(containerGroup, containerPerson);
-    namePlaceholder.innerHTML = '';
-    newWordList.innerHTML = '';
-});
 
 function startNewPerson() {
     hiddenDiv(containerPerson, containerGroup);
@@ -36,14 +30,21 @@ function hiddenDiv(remove, add) {
     add.classList.add('hidden');
 }
 
+
+
 bttnName.addEventListener('click', (e) => {
     if (inputName.placeholder.includes('your name')) {
         currentPerson = inputName.value;
         namePlaceholder.innerHTML = currentPerson;
+        peopleArray.push(inputName.value);
+
         inputName.value = '';
         inputName.placeholder = 'add a phrase';
 
         phraseArray.push({ name: currentPerson, phrases: [] });
+
+        console.log(peopleArray);
+        
     } else {
         const personObject = phraseArray.find(obj => obj.name === currentPerson);
         if (personObject) {
@@ -60,4 +61,22 @@ bttnName.addEventListener('click', (e) => {
     }
 
     console.log(phraseArray);
+});
+
+
+
+bttnPersonDone.addEventListener('click', () => {
+
+    currentPerson = null;
+    hiddenDiv(containerGroup, containerPerson);
+    namePlaceholder.innerHTML = '';
+    newWordList.innerHTML = '';
+
+    if (peopleArray){
+        peopleArray.forEach(person => {
+            const createNameText = document.createElement('p');
+            createNameText.innerHTML = person;
+            nameList.appendChild(createNameText);
+        });
+    }
 });
